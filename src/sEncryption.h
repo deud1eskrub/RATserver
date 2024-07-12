2f4fabdba310728095c3a5e0d329afa7a3b80f29
deud1eskrub <61610218+deud1eskrub@users.noreply.github.com>
2024-07-11 20:55:27 -0400

    Update sEncryption.h

--- src/sEncryption.h
+++ src/sEncryption.h
@@ -4,3 +4,94 @@
 
 
 void __cdecl zeroBuffer(char*, int);
+
+constexpr unsigned int LOWER_lowerBound = 65;
+constexpr unsigned int LOWER_upperBound = 90;
+constexpr unsigned int CAPS_lowerBound = 97;
+constexpr unsigned int CAPS_upperBound = 122;
+constexpr unsigned int NUMS_lowerBound = 48;
+constexpr unsigned int NUMS_upperBound = 57;
+constexpr unsigned int SPACE = 32;
+
+bool isValidChar(unsigned char byte)
+{
+	if ((byte >= LOWER_lowerBound && byte <= LOWER_upperBound) || (byte >= CAPS_lowerBound && byte <= CAPS_upperBound) || (byte >= NUMS_lowerBound && byte <= NUMS_upperBound) || byte==SPACE)
+	{
+		return true;
+	}
+	return false;
+}
+std::vector<std::string> ParseStrings(char* bytes, unsigned int size, unsigned int criteria)
+{
+	std::vector<std::string> ret;
+	unsigned int i, j, criteriaMatched;
+	
+	bool* mask = new bool[size];
+	memset(mask, 0, size);
+	for (i = 0; i < size; i++)
+	{
+		mask[i] = isValidChar(bytes[i]);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (mask[i])
+		{
+			for (j = 0; i+j<=size; j++)
+			{
+				if (!mask[i + j])
+				{
+					break;
+				}
+			}
+			if (j >= criteria)
+			{
+				ret.push_back(std::string(bytes + i, j));
+				i += j;
+			}
+		}
+	}
+	delete[] mask;
+	return ret;
+}
+
+
+
+int main(int argc, char**argv)
+{
+	//if (argc == 1)
+	//{
+	//	std::cout << "No input";
+	//	return 0;
+	//}
+
+	//int sensitivityCriteria;
+	//std::string blahblah = std::string(argv[2]);
+	//std::stringstream parser(blahblah);
+	//parser >> sensitivityCriteria;
+	
+	char* bytes;
+	unsigned long long increment = 0, sizeOnDisk = 0;
+	//"C:\\Users\\Falcon\\Downloads\\X670EAORUSMASTER.F32a"
+	std::ifstream binary(/*argv[1]*/"C:\\Users\\Falcon\\Downloads\\X670EAORUSMASTER.F32a", std::ifstream::binary);
+	binary.seekg(0, binary.end);
+	sizeOnDisk = binary.tellg();
+	std::cout << sizeOnDisk << std::endl;
+	binary.seekg(0, binary.beg);
+
+	bytes = new char[sizeOnDisk];
+	memset(bytes, 0, sizeOnDisk);
+
+	while (binary.good())
+	{
+		binary.read(bytes + increment, 4096);
+		increment += 4096;
+	};
+
+	std::vector<std::string> strs = ParseStrings(bytes, sizeOnDisk, 6/*sensitivityCriteria*/);
+	for(int i = 0; i < strs.size(); i++)
+	{
+		std::cout << strs[i] << "\n";
+	}
+	
+	return 0;
+}
